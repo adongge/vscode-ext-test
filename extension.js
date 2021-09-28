c67c0f8699e6f1c2346fd4753a8b567832354815
adong <adddge@sohu.com>
2021-09-28 16:02:08 +0800

    20210928

--- extension.js
+++ extension.js
@@ -1,41 +1,31 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "test" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('test.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	const helpTreeView = vscode.window.createTreeView('test.testTree',{ new vscode.TreeView(FUnction(){});  });
+	helpTreeView.push()
+    context.subscriptions.push(helpTreeView);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from test!');
-	});
-
-	const outputConsole = vscode.window.createOutputChannel('Laravel Artisan')
-	outputConsole.appendLine('[CMD] test adong line ')
-
-	const terminal = vscode.window.createTerminal('test');//创建终端
-	terminal.show();
-	terminal.sendText('php -m');
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('test.helloWorld', function () {
+			const myt = vscode.window.activeTerminal;
+			if(myt){
+				myt.show();
+				myt.sendText('php -i');
+			}else{
+				const terminal = vscode.window.createTerminal();
+				terminal.show();
+				terminal.sendText('php -m');
+			}
+		})
+	);
 }
-
 // this method is called when your extension is deactivated
-function deactivate() { }
+function deactivate() {}
 
 module.exports = {
 	activate,
